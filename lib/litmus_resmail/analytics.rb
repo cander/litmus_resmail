bb376e4e6866a9f722cf59b10a3ec95bcb91b398
Charles Anderson <master.sparkle@gmail.com>
2012-02-03 11:22:56 -0800

    First steps towards real functionality
    Added savon gem to connect to SOAP for real.

--- lib/litmus_resmail/analytics.rb
+++ lib/litmus_resmail/analytics.rb
@@ -1,7 +1,18 @@
 require 'hashie'
+require 'savon'
 
 module LitmusResmail
   class Analytics
+    def initialize(user, password, wsdl_file = nil)
+      @user = user
+      @password = password
+      # could make this a factory method to aid in mocking/stubbing
+      @client = Savon::Client.new do
+         # set the wsdl.endpoint if no file
+         wsdl.document = wsdl_file if wsdl_file
+      end
+    end
+
     # Conflusingly (by their own admission), what most people would call a
     # campaign, Litmus calls a "report".
     def create()
@@ -9,6 +20,27 @@ module LitmusResmail
                                 :guid => Time.now.to_s,
                                 :reportGuid => "report of #{Time.now}",
                                 :individual => false)
+    end
+
+    def get_engagement_report(guid)
+    end
+
+    def do_request(method, arg_hash = {})
+      args = { :user => @user, :password => @password }
+      args.merge!(arg_hash)
+      puts "calling #{method} with #{args.inspect}"
+      response = @client.request(method) do
+        soap.body = args
+      end
+
+      # not sure what more to do for error checking, yet
+      return response if response.to_xml.empty?       # happens when mocking with savon-spec
+
+      # strip off method_response and method_result wrappers
+      resp_key = "#{method.to_s.snakecase}_response".to_sym
+      res_key = "#{method.to_s.snakecase}_result".to_sym
+
+      response[resp_key][res_key]
     end
   end
 end
--- spec/litmus_resmail_spec.rb
+++ spec/litmus_resmail_spec.rb
@@ -1,4 +1,24 @@
 require 'litmus_resmail'
+require 'savon_spec'
+
+# TODO: put this config stuff in a helper file
+RSpec.configure do |config|
+  config.include Savon::Spec::Macros
+  config.backtrace_clean_patterns = [
+      /\/lib\d*\/ruby\//,
+      /bin\//,
+      #/gems/,
+      /spec\/spec_helper\.rb/,
+      /lib\/rspec\/(core|expectations|matchers|mocks)/
+      ]
+end
+Savon.configure do |config|
+  config.log = false    # turn off logging - for tests
+end
+fixtures = File.join(File.dirname(__FILE__), "fixtures")
+Savon::Spec::Fixture.path = fixtures
+litmus_wsdl = File.join(fixtures, 'litmus_wsdl.xml')
+
 
 describe LitmusResmail::Analytics do
   it 'create should create a new report' do
@@ -6,5 +26,20 @@ describe LitmusResmail::Analytics do
     report = api.create
     report.should_not be_nil
     report.bugHtml.should_not be_nil
+  end
+
+  describe '#do_request' do
+    let(:api) { LitmusResmail::Analytics.new('user', 'pw', litmus_wsdl) }
+
+    it 'should call method and pass arguments' do
+      savon.expects('GetEngagementReport').with(:user => 'user', :password => 'pw', :arg => 'some arg').returns
+      api.do_request('GetEngagementReport', :arg => 'some arg')
+    end
+
+    it 'should peel off nested ressults' do
+      savon.stubs('GetEngagementReport').returns(:all_zeroes)
+      result = api.do_request('GetEngagementReport')
+      result.should include(:glanced_or_unread_count)
+    end
   end
 end 
